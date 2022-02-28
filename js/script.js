af23fa6319de3f29980bbc7a26cc174e601c76d5
lucasmarianokotcz <lucasmariano-@hotmail.com>
2022-02-28 01:39:21 -0300

    Correções "null"

--- js/script.js
+++ js/script.js
@@ -10,14 +10,14 @@ function showGithubUsers() {
                 document.getElementById('errorGit').innerHTML = `<h3>Profile Not Found</h3>`
             } else {
                 console.log(data)
-                document.getElementById('profileImg').innerHTML = `<img src="${data.avatar_url}" style="width:200px">`
-                document.getElementById('bioGit').innerHTML = `<p>${data.bio}</p>`
-                document.getElementById('nameGit').innerHTML = `<p>${data.name}</p>`
+                document.getElementById('profileImg').innerHTML = data.avatar_url ? `<img src="${data.avatar_url}" style="width:200px">` : document.getElementById('profileImg').innerHTML
+                document.getElementById('bioGit').innerHTML = `<p>${data.bio ? data.bio : '<i>Biografia não informada</i>'}</p>`
+                document.getElementById('nameGit').innerHTML = `<p>${data.name ? data.name : '<i>Nome não informado</i>'}</p>`
                 document.getElementById('userGit').innerHTML = `<p>${data.login}</p>`
-                document.getElementById('followersGit').innerHTML = `<i class="fas fa-user-friends"></i> <p>${data.followers} followers</p> <i class="fas fa-user-friends"></i> <p>${data.following} following</p>`
-                document.getElementById('reposGit').innerHTML = `<i class="fas fa-laptop"></i><p>${data.public_repos} Repositorios</p>`
-                document.getElementById('locationGit').innerHTML = `<i class="fas fa-map-marker-alt"></i> <p>${data.location}</p>`
-                document.getElementById('blogGit').innerHTML = `<i class="fas fa-link"></i>  <a href="${data.blog}" target="_blank">${data.blog}</a>`
+                document.getElementById('followersGit').innerHTML = `<i class="fas fa-user-friends"></i> <p>${data.followers} seguidores</p> &ensp; <i class="fas fa-user-friends"></i> <p>${data.following} seguindo</p>`
+                document.getElementById('reposGit').innerHTML = `<i class="fas fa-laptop"></i><p>${data.public_repos} repositórios</p>`
+                document.getElementById('locationGit').innerHTML = `<i class="fas fa-map-marker-alt"></i> <p>${data.location ? data.location : '<i>Localização não informada</i>'}</p>`
+                document.getElementById('blogGit').innerHTML = `<i class="fas fa-link"></i> ${data.blog ? `<a href="${data.blog}" target="_blank">${data.blog}</a>` : '<i>Link não informado</i>' }`
 
             }
         }).catch(e => {
