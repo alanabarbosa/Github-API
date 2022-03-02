334e9eece86fa815268fdb8da466dbc59f8fc8c3
alanabarbosa <itsalanayall@hotmail.com>
2022-03-02 08:42:17 -0300

    Arruma bug de profle not found

--- js/script.js
+++ js/script.js
@@ -6,18 +6,28 @@ function showGithubUsers() {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            if (data.message) {
-                document.getElementById('errorGit').innerHTML = `<h3>Profile Not Found</h3>`
-            } else {
-                console.log(data)
-                document.getElementById('profileImg').innerHTML = data.avatar_url ? `<img src="${data.avatar_url}" style="width:200px">` : document.getElementById('profileImg').innerHTML
-                document.getElementById('bioGit').innerHTML = `<p>${data.bio ? data.bio : '<i>Biografia não informada</i>'}</p>`
-                document.getElementById('nameGit').innerHTML = `<p>${data.name ? data.name : '<i>Nome não informado</i>'}</p>`
-                document.getElementById('userGit').innerHTML = `<p>${data.login}</p>`
-                document.getElementById('followersGit').innerHTML = `<i class="fas fa-user-friends"></i> <p>${data.followers} seguidores</p> &ensp; <i class="fas fa-user-friends"></i> <p>${data.following} seguindo</p>`
-                document.getElementById('reposGit').innerHTML = `<i class="fas fa-laptop"></i><p>${data.public_repos} repositórios</p>`
-                document.getElementById('locationGit').innerHTML = `<i class="fas fa-map-marker-alt"></i> <p>${data.location ? data.location : '<i>Localização não informada</i>'}</p>`
-                document.getElementById('blogGit').innerHTML = `<i class="fas fa-link"></i> ${data.blog ? `<a href="${data.blog}" target="_blank">${data.blog}</a>` : '<i>Link não informado</i>' }`
+                if (data.message) {
+                    document.getElementById('errorGit').style.display = 'block'
+                    document.getElementById('errorGit').innerHTML = `<h3>Profile Not Found</h3>`
+                    document.getElementById('profileImg').innerHTML = ''
+                    document.getElementById('bioGit').innerHTML = ''
+                    document.getElementById('nameGit').innerHTML = ''
+                    document.getElementById('userGit').innerHTML = ''
+                    document.getElementById('followersGit').innerHTML = ''
+                    document.getElementById('reposGit').innerHTML = ''
+                    document.getElementById('locationGit').innerHTML = ''
+                    document.getElementById('blogGit').innerHTML = ''
+                } else {
+                    console.log(data)
+                    document.getElementById('errorGit').style.display = 'none'
+                    document.getElementById('profileImg').innerHTML = data.avatar_url ? `<img src="${data.avatar_url}" style="width:200px">` : document.getElementById('profileImg').innerHTML
+                    document.getElementById('bioGit').innerHTML = `<p>${data.bio ? data.bio : '<i>Biografia não informada</i>'}</p>`
+                    document.getElementById('nameGit').innerHTML = `<p>${data.name ? data.name : '<i>Nome não informado</i>'}</p>`
+                    document.getElementById('userGit').innerHTML = `<p>${data.login}</p>`
+                    document.getElementById('followersGit').innerHTML = `<i class="fas fa-user-friends"></i> <p>${data.followers} seguidores</p> &ensp; <i class="fas fa-user-friends"></i> <p>${data.following} seguindo</p>`
+                    document.getElementById('reposGit').innerHTML = `<i class="fas fa-laptop"></i><p>${data.public_repos} repositórios</p>`
+                    document.getElementById('locationGit').innerHTML = `<i class="fas fa-map-marker-alt"></i> <p>${data.location ? data.location : '<i>Localização não informada</i>'}</p>`
+                    document.getElementById('blogGit').innerHTML = `<i class="fas fa-link"></i> ${data.blog ? `<a href="${data.blog}" target="_blank">${data.blog}</a>` : '<i>Link não informado</i>' }`
 
             }
         }).catch(e => {
